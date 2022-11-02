642f77136010fc361bc30a011948b87015c4ef1a
Filip Wesolowski <filip.wesolowski@workflows.se>
2022-11-02 02:30:09 +0100

    new tasks structure

--- index.js
+++ index.js
@@ -7,15 +7,20 @@ const fs = require('fs');
 app.use(express.json());
 app.use(cors());
 
-app.get('/', (req, res) => {
+app.get('/', (_req, res) => {
   const data = JSON.parse(fs.readFileSync('data.json', 'utf-8'));
   res.send(data.tasks);
 });
 
 app.post('/', (req, res) => {
   const data = JSON.parse(fs.readFileSync('data.json', 'utf-8'));
-  if (data.tasks.find(e => e === req.body.task)) return res.status(400).send('Task already exist');
-  data.tasks.push(req.body.task);
+  if (data.tasks.find(e => e.name === req.body.task)) return res.status(400).send('Task already exist');
+  data.tasks.push({
+    name: req.body.task,
+    checked: false,
+    id: data.tasks.length ? data.tasks[data.tasks.length - 1].id + 1 : 0
+  });
+  console.log(data);
   fs.writeFileSync('data.json', JSON.stringify(data));
   res.status(200).send('Task added');
 });
